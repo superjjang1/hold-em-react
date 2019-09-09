a7a5338d2f6a445747edc28caa22ea01fe9d1e3d
Sean Yim <superjjang89@gmail.com>
2019-09-09 13:52:06 -0400

    skeleton

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import PokerTable from './components/PokerTable';
+
+function App() {
+  return (
+    <div className="App">
+      <PokerTable />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PokerTable.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class PokerTable extends Component {
+    render(){
+        return(
+            <h1>Poker Table</h1>
+        )
+    }
+}
+
+export default PokerTable;
