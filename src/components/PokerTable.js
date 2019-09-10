b6fddcf465f8bc46c39158ff36054174b7293a18
Sean Yim <superjjang89@gmail.com>
2019-09-10 13:22:52 -0400

    working solver

--- src/components/PokerTable.js
+++ src/components/PokerTable.js
@@ -1,27 +1,80 @@
 import React, { Component } from 'react';
+import './PokerTable.css';
 import PokerHand from './PokerHand';
 import Deck from '../utilityClasses/Deck';
-import './PokerTable.css';
 
-class PokerTable extends Component{
-    constructor(){
+class PokerTable extends Component {
+    constructor() {
         super();
-        this.deck = new Deck(); //make a new Deck object 
-        this.deck.create(); // create a deck from the Deck object
-        this.deck.shuffle(); // shuffle the deck
-        console.log(this.deck.cards);
+        this.deck = new Deck();
+        this.deck.create();
+        this.deck.shuffle();
+        // console.log(this.deck.cards)
         this.state = {
-            playerHand: ['deck','deck'], //this is the player array of cards
-            dealerHand: ['deck','deck'],
-            communityHand: ['deck','deck','deck','deck','deck'],
-            wager: 0,
-            bankroll: 100
+            dealerHand: ['deck', 'deck'],
+            playerHand: ['deck', 'deck'],
+            communityHand: [],
+            wager: 0, 
+            bankroll: 100,
+            msg:''
         }
     }
 
-    // this method is made by us! Not a "react" method
-    // in here we deal the first 4 cards
-    prepDeck = ()=>{
+    checkHandRank = () => {
+        const playerPlusComm = this.pokerfyHand([...this.state.playerHand, ...this.state.communityHand]);
+        const dealerPlusComm = this.pokerfyHand([...this.state.dealerHand, ...this.state.communityHand]);
+        const playerHandRank = window.Hand.solve(playerPlusComm);
+        const dealerHandRank = window.Hand.solve(dealerPlusComm);
+        console.log(playerHandRank);
+        console.log(dealerHandRank);
+
+        this.findWinner(playerHandRank, dealerHandRank);
+    }
+
+    findWinner = (hand1, hand2) => {
+        const winnerArr = window.Hand.winners([hand1, hand2]);
+        console.log(winnerArr);
+        console.log(winnerArr.length > 1)
+        console.log(winnerArr[0].descr === hand1.descr)
+        console.log(winnerArr[0].descr === hand2.descr)
+        console.log(winnerArr[0].descr === hand1.descr && winnerArr[0].descr === hand2.descr)
+        if (winnerArr.length > 1 || (winnerArr[0].descr === hand1.descr && winnerArr[0].descr === hand2.descr)) {
+            this.setState( {
+                msg: 'Game is a Draw'
+            }, this.clearMsg)
+        } else if ( winnerArr[0].descr === hand1.descr) {
+            this.setState( {
+                msg: 'Player Wins!'
+            }, this.clearMsg)
+        } else if (winnerArr[0].descr === hand2.descr) {
+            this.setState( {
+                msg: 'Dealer Wins!'
+            }, this.clearMsg)
+        }
+    }
+
+    pokerfyHand = (hand) => {
+        const newHand = hand.map( card => {
+            if( card.substring(0,2) === '10') {
+                return ('T'+card[2])
+            } else if ( card.substring(0,2) === '11') {
+                return ('J'+card[2])
+            } else if ( card.substring(0,2) === '12') {
+                return ('Q'+card[2])
+            } else if ( card.substring(0,2) === '13') {
+                    return ('K'+card[2])
+            } else if ( card[0] === '1') {
+                return ('A'+card[1])
+            } else {
+                return card
+            }
+        })
+        return newHand
+    }
+
+
+    prepDeck = () => {
+        this.deck = new Deck();
         this.deck.create();
         this.deck.shuffle();
         // const burn = this.deck.cards.shift();
@@ -29,78 +82,99 @@ class PokerTable extends Component{
         const card2 = this.deck.cards.shift();
         const card3 = this.deck.cards.shift();
         const card4 = this.deck.cards.shift();
-        // at this point, this.deck.cards has 48 elements in it
-        // because we removed 4
-        this.setState({
-            playerHand: [card1,card3],
-            dealerHand: [card2,card4]
-        })
-    };
 
-    bet = (amount)=>{
-        // NO NO NO NO NO NO NO NO NO
-        // this.state.wager += amount;
+        this.setState({
+            dealerHand: [card1, card3],
+            playerHand: [card2, card4],
+            communityHand:[]
+        })
+    }
+
+    bet = (amount) => {
         const newWager = this.state.wager + amount;
         const newBankRoll = this.state.bankroll - amount;
-        if(newBankRoll >= 0){
+        if (newBankRoll >= 0) {
             this.setState({
-                wager: newWager,
+                wager:newWager,
                 bankroll: newBankRoll
             })
-        }else{
+        } else {
             this.setState({
-                msg: "You NEED TO WIN!?!?!?"
-            },this.clearMsg)
+                msg: "You don't have enough money!"
+            }, this.clearMsg)
         }
     }
 
-    clearMsg = ()=>{
-        setTimeout(()=>{
-            this.setState({
-                msg:""
-            })
-        },2000)
+    clearMsg = () => {
+        setTimeout( () => { this.setState({ msg: "" })}, 2000)
     }
-    //run check calls to draw a new community card
-    // 1. 3 cards flop, 2. 1 card turn, 3. 1 card river
-    check = () =>{
-        // console.log('user clicked check');
-        // we do not want to mutate or change state
-        //we make a copy, do stuff to copy and hand to set state
-        
+
+    check = () => {
         let communityNewHand = [...this.state.communityHand];
+        if (this.state.dealerHand[0] === 'deck' || communityNewHand.length === 5) {
 
+        } else if (communityNewHand.length === 0 ) {
+            communityNewHand = [    
+                this.deck.cards.shift(),
+                this.deck.cards.shift(),
+                this.deck.cards.shift()
+            ]
+        } else {
+            communityNewHand.push(this.deck.cards.shift())
+        }
+        this.setState({
+            communityHand: communityNewHand
+        }, this.fullCommunityHand)
+    }
+
+    fullCommunityHand = () => {
+        console.log(this.state.communityHand)
+        if ( this.state.communityHand.length === 5 ) {
+            this.checkHandRank();
+        }
     }
 
 
-    render(){
-        return(
-            <div className="the-table col-sm-12">
-                <div className="col-sm-12 text-center the-numbers">
-                    <div className="col-sm-3 col-sm-offset-3">
+
+    render() { 
+        // console.log(this.state.dealerHand)
+        // console.log(this.state.playerHand)
+        // console.log(this.state.communityHand)
+        return (
+            <div className='the-table col-sm-12'> 
+                <div className='col-sm-12 text-center the-numbers'>
+                    <div className='col-sm-4 col-sm-offset-1'>
                         Current Pot: ${this.state.wager}
                     </div>
-                    <div className="col-sm-3">
+                    <div className='col-sm-4 col-sm-offset-2'>
                         Bankroll: ${this.state.bankroll}
                     </div>
                 </div>
-                <div className="player-message">
-                    {this.state.msg}                    
+                <div className='player-message'>
+                    {this.state.msg}
                 </div>
-
-                <PokerHand cards={this.state.playerHand} />   {/* Player 1 hand */}
-                <PokerHand cards={this.state.communityHand} />   {/* Community hand */}
-                <PokerHand cards={this.state.dealerHand} />   {/* Player 2 hand */}
-                <div className="col-sm-12 buttons">
-                    <button onClick={this.prepDeck} className="btn btn-primary">Deal</button>
-                    <button onClick={()=>{this.bet(5)}} className="btn btn-success">Bet 5</button>
-                    <button onClick={this.prepDeck} className="btn btn-warning">Check</button>
-                    <button onClick={this.prepDeck} className="btn btn-danger">Fold</button>
+                <h1>Dealer Hand:</h1>
+                <PokerHand cards={this.state.dealerHand} />
+                <PokerHand cards={this.state.communityHand} />
+                <h1>Player Hand:</h1>
+                <PokerHand cards={this.state.playerHand} />
+                <div className='col-sm-12 buttons'>
+                    <button onClick={this.prepDeck} className='btn btn-primary'>
+                        Deal
+                    </button>
+                    <button onClick={() => {this.bet(5)}} className='btn btn-success'>
+                        Bet 5
+                    </button>
+                    <button onClick={this.check} className='btn btn-warning'>
+                        Check
+                    </button>
+                    <button onClick={this.prepDeck} className='btn btn-danger'>
+                        Fold
+                    </button>
                 </div>
             </div>
-        )
+        );
     }
 }
-
-
-export default PokerTable
+ 
+export default PokerTable;
